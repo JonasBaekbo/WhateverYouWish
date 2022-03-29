26fec921d895e88af46f1be95fa5a8f814abffe3
JonasBaekbo <jonas.bryde.baekbo@live.dk>
2022-03-29 12:10:13 +0200

    Login system working

---
+++ src/main/java/com/example/whateveryouwish/ApplicationConfig.java
@@ -0,0 +1,55 @@
+package com.example.whateveryouwish;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("pass")
+                .successForwardUrl("/login_success").successHandler(new AuthenticationSuccessHandler() {
+
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+
+                        System.out.println("Logged user: " + authentication.getName());
+
+                        response.sendRedirect("/test");
+                    }
+                });
+
+    }
+
+
+}
---
+++ src/main/java/com/example/whateveryouwish/MainController.java
@@ -0,0 +1,20 @@
+package com.example.whateveryouwish;
+
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/test")
+    public String test(Model m){
+
+        return "test";
+    }
+}
